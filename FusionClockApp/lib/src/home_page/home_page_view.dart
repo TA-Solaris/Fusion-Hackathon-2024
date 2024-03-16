9682ef543bcc94d6cd50f25e9ecf335b64fff8c9
Robert3141 <robert.aries1@gmail.com>
2024-03-16 20:49:18 +0000

    Persist Alarm Times

--- FusionClockApp/lib/src/home_page/home_page_view.dart
+++ FusionClockApp/lib/src/home_page/home_page_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:fusionclock/src/accounts_pages/register_page_view.dart';
 import 'package:fusionclock/src/alarm_page/alarm_page_time.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../settings/settings_view.dart';
 import '../alarm_page/alarm_page_view.dart';
@@ -20,6 +21,36 @@ class HomePageState extends State<HomePageView> {
 
   String formatTime() {
     return "${alarmTime.hour.toString().padLeft(2, '0')}:${alarmTime.minute.toString().padLeft(2, '0')}";
+  }
+
+  void alarmUpdated() async {
+    var tempTime =
+        await showTimePicker(context: context, initialTime: alarmTime) ??
+            alarmTime;
+    setState(() {
+      alarmTime = tempTime;
+    });
+
+    //Save alarm time
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('alarm_hour', alarmTime.hour);
+    await prefs.setInt('alarm_min', alarmTime.minute);
+  }
+
+  void loadPrefs() async {
+    //Load alarm time
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    int hours = prefs.getInt('alarm_hour') ?? 7;
+    int mins = prefs.getInt('alarm_min') ?? 30;
+    setState(() {
+      alarmTime = TimeOfDay(hour: hours, minute: mins);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadPrefs();
   }
 
   @override
@@ -57,14 +88,7 @@ class HomePageState extends State<HomePageView> {
         children: [
           const Center(child: AlarmPageTime()),
           ElevatedButton(
-              onPressed: () async {
-                var tempTime = await showTimePicker(
-                        context: context, initialTime: alarmTime) ??
-                    alarmTime;
-                setState(() {
-                  alarmTime = tempTime;
-                });
-              },
+              onPressed: alarmUpdated,
               child: Text(
                 "Alarm: ${formatTime()}",
                 style: const TextStyle(
