410ef973eb357765f985973981cc5ca1b9f903c2
Robert3141 <robert.aries1@gmail.com>
2024-03-17 04:07:21 +0000

    Users are now forced to login: commented out checkAuth(); in HomePage build

--- FusionClockApp/lib/src/accounts_pages/register_page_view.dart
+++ FusionClockApp/lib/src/accounts_pages/register_page_view.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:fusionclock/src/accounts_pages/signin_page_view.dart';
 import 'package:fusionclock/src/backend_interface/backend.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 import '../ErrorBar/error_bar.dart';
 import '../home_page/home_page_view.dart';
 import 'decorated_field.dart';
 
 class RegisterPageView extends StatefulWidget {
-  const RegisterPageView({Key? key}) : super(key: key);
+  const RegisterPageView({super.key});
 
   static const routeName = '/register';
 
@@ -23,8 +21,8 @@ class RegisterPageState extends State<RegisterPageView> with BackEnd {
   final password2TextController = TextEditingController();
 
   Future<bool> submitButton() async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    var result = await register(emailTextController.text, passwordTextController.text);
+    var result =
+        await register(emailTextController.text, passwordTextController.text);
     return result;
   }
 
@@ -41,8 +39,8 @@ class RegisterPageState extends State<RegisterPageView> with BackEnd {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                SizedBox(height: 40),
-                Text(
+                const SizedBox(height: 40),
+                const Text(
                   "Sign Up",
                   textAlign: TextAlign.center,
                   style: TextStyle(
@@ -76,17 +74,17 @@ class RegisterPageState extends State<RegisterPageView> with BackEnd {
                   child: ElevatedButton(
                     onPressed: () => {
                       submitButton().then((value) => {
-                        if (!value)
-                          {
-                            // Error notification
-                            showFlashError(context, "Registration failed")
-                          }
-                        else
-                          {
-                            Navigator.pushNamed(
-                                context, HomePageView.routeName)
-                          }
-                      })
+                            if (!value)
+                              {
+                                // Error notification
+                                showFlashError(context, "Registration failed")
+                              }
+                            else
+                              {
+                                Navigator.pushNamed(
+                                    context, HomePageView.routeName)
+                              }
+                          })
                     },
                     style: ElevatedButton.styleFrom(
                         shape: const StadiumBorder(),
@@ -94,29 +92,6 @@ class RegisterPageState extends State<RegisterPageView> with BackEnd {
                         backgroundColor: themeColor),
                     child: const Text('Sign Up',
                         style: TextStyle(fontSize: 20, color: Colors.white)),
-                  ),
-                ),
-                const SizedBox(height: 20),
-                Center(
-                  child: Text(
-                    "OR",
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Container(
-                  padding: const EdgeInsets.only(top: 3, left: 3),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushReplacementNamed(
-                          context, LoginPageView.routeName);
-                    },
-                    style: ElevatedButton.styleFrom(
-                        shape: const StadiumBorder(),
-                        padding: const EdgeInsets.symmetric(vertical: 8),
-                        backgroundColor: themeColor.shade100),
-                    child: Text('Login',
-                        style:
-                            TextStyle(fontSize: 20, color: themeColor.shade900)),
                   ),
                 ),
                 const SizedBox(height: 20),
