0e2c778825d630812258d92d9ac558c344f25935
Edward Potter <74555065+TA-Solaris@users.noreply.github.com>
2024-03-17 04:24:06 +0000

    Made styling better for mobile friends

--- FusionClockApp/lib/src/friends/friend_widget.dart
+++ FusionClockApp/lib/src/friends/friend_widget.dart
@@ -17,19 +17,19 @@ class FriendTile extends StatelessWidget {
   Widget build(BuildContext context) {
     List<Widget> row = [
       Padding(
-        padding: const EdgeInsets.all(20),
+        padding: const EdgeInsets.fromLTRB(4, 24, 8, 24),
         child: ElevatedButton(
             style: ElevatedButton.styleFrom(
                 shape: const CircleBorder(),
-                fixedSize: const Size.fromRadius(50),
-                padding: const EdgeInsets.only(left: 10, right: 10)),
+                fixedSize: const Size.fromRadius(40),
+                padding: const EdgeInsets.only(left: 8, right: 8)),
             onPressed: () {},
             child: const Text(
               "",
             )),
       ),
       const SizedBox(
-        width: 20,
+        width: 10,
       ),
       Expanded(
           child: Text(
@@ -43,29 +43,33 @@ class FriendTile extends StatelessWidget {
 
     //Friend request buttons
     if (friendRequest) {
-      row.addAll([
-        ElevatedButton(
-            style: ElevatedButton.styleFrom(
+      row.add(
+        Column(
+          children: [
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
                 shape: const CircleBorder(),
                 padding: const EdgeInsets.only(),
-                fixedSize: const Size.fromRadius(25)),
-            onPressed: () {
-              if (onAccept != null) onAccept!();
-            },
-            child: const Icon(Icons.done)),
-        const SizedBox(
-          width: 20,
-        ),
-        ElevatedButton(
-            style: ElevatedButton.styleFrom(
+                fixedSize: const Size.fromRadius(15)),
+              onPressed: () {
+                if (onAccept != null) onAccept!();
+              },
+              child: const Icon(Icons.done)),
+            const SizedBox(
+              height: 4,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
                 shape: const CircleBorder(),
                 padding: const EdgeInsets.only(),
-                fixedSize: const Size.fromRadius(25)),
-            onPressed: () {
-              if (onReject != null) onReject!();
-            },
-            child: const Icon(Icons.close))
-      ]);
+                fixedSize: const Size.fromRadius(15)),
+              onPressed: () {
+                if (onReject != null) onReject!();
+              },
+              child: const Icon(Icons.close))
+          ]
+        )
+      );
     }
 
     return InkWell(
